34ce5a413bf295389f4056cab423fc52f69e0eed
smilliononline <million_18@outlook.com>
2018-09-26 05:13:15 -0500

    update

--- js/script.js
+++ js/script.js
@@ -32,11 +32,13 @@ function printQuote(){
    
 
     document.getElementById('quote-box').innerHTML = htmlStrings; // writes quote
-    
+
 }
 
-printQuote();
+
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+
+    
